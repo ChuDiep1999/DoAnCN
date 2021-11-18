6f823bc66cfa45aa98724fcc7eabf94239a6f5e0
chuvandiep1999 <chuvandiep1999@gmail.com>
2021-11-18 12:39:16 +0700

    DangNhap

--- DoAnChuyenNganh/DoAnChuyenNganh/Controllers/HomeController.cs
+++ DoAnChuyenNganh/DoAnChuyenNganh/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DoAnChuyenNganh.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +9,47 @@ namespace DoAnChuyenNganh.Controllers
 {
     public class HomeController : Controller
     {
+        DataDoAnChuyenNganhEntities db = new DataDoAnChuyenNganhEntities();
         public ActionResult Index()
         {
-            return View();
+            if (Session["TaiKhoan"] != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("DangNhap");
+            }
+
         }
 
-        public ActionResult About()
+        public ActionResult DangNhap()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
-
-        public ActionResult Contact()
+        [HttpPost]
+        public ActionResult DangNhap(FormCollection f)
         {
-            ViewBag.Message = "Your contact page.";
-
+            string taikhoan = f["txtTenDangNhap"].ToString();
+            string matkhau = f["txtMatKhau"].ToString();
+            var data = db.ThanhViens.Where(s => s.TenDangNhap.Equals(taikhoan) && s.MatKhau.Equals(matkhau)).ToList();
+            ThanhVien tv = db.ThanhViens.SingleOrDefault(n => n.TenDangNhap  == taikhoan && n.MatKhau == matkhau);
+            if (tv != null)
+            {
+                Session["TaiKhoan"] = tv;
+                Session["HoTen"] = data.FirstOrDefault().HoTen;
+                return View("Index");
+                
+            }
+            ViewBag.ThongBaoDangNhap = "Thông tin đăng nhập không chính xác";
             return View();
+        }
+        public ActionResult DangXuat()
+        {
+            Session.Clear();//remove session
+            return RedirectToAction("DangNhap");
         }
     }
+   
+
 }
